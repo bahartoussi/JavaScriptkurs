243ef324630c1cd9e768483ed5a2e9ab674ea517
unknown <bahar.toussi@gmail.com>
2023-09-08 15:42:32 +0200

    Datentypen Operatoren

---
+++ js/datentypen.js
@@ -0,0 +1,32 @@
+console.log("datentypenjs ist da")
+// zahlen sind vom typ number
+let zahl1=1.0;
+let zahl2=2.0;
+zahl1=zahl1+zahl2;
+console.log("zahl1 ist "+zahl1);
+let text1="text1"
+let text2="text2"
+
+console.log(text1+text2);
+console.log(text1/5);
+// NaN not a  number 
+console.log(zahl1/5);
+console.log(Math.floor(zahl1/5));
+//zahl1="text";
+zahl1=zahl1+zahl2;
+console.log(zahl1);
+let falsetext="";  // im Boolschen Bereich wird das zu false
+let boolvar1=false;
+let boolvar2= true;
+boolvar1=true;
+let zahl3;
+console.log(typeof zahl3);
+console.log(typeof text1);
+text1="hallo wie gehts"; 
+    parseInt("text");
+console.log(text1*zahl2);
+text1="20";
+let erg= text1*zahl2;
+console.log(erg);
+let text4='Der begriff \'ist\' "begriff"';
+console.log(text4);
---
+++ js/operatoren.js
@@ -0,0 +1,11 @@
+/*Arithmetischen Operatoren
+
++,-,*,/, %
+
+%  Restwertoperator Modulooperator
+
+*/
+
+console.log("ausgabe"+(8%3));
+let zahl1=12;
+console.log(zahl1%2==1);
