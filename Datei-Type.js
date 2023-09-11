76b72a0cd1bba24c34deff3d24b0445cfac340d0
bahartoussi <118759613+bahartoussi@users.noreply.github.com>
2023-09-11 19:47:54 +0200

    Add files via upload

---
+++ Datei-Type.js
@@ -0,0 +1,14 @@
+var i=6;
+var k;
+var l=11;
+var j=true;
+var s='sara';
+
+//document.write('ich befinde mich in einer externen Datei.');
+console.log('variable i hat den Wert :',i);
+console.log("variable k hat den Wert :",k);
+console.log("variable l hat den Wert :",l);
+console.log("variable i hat den type: " ,typeof i);
+console.log("variable k hat den type: " ,typeof k);
+console.log("variable s hat den type: ",typeof s);
+console.log("variable j hat den type: ",typeof j);
---
+++ Datei-Type2.js
@@ -0,0 +1,18 @@
+var i=6;
+var k;
+var l=11;
+var j=true;
+var s='sara';// could i define this variable as "let"
+
+//document.write('ich befinde mich in einer externen Datei.');
+document.write('variable i hat den Wert :',i)+"<br>"
+document.write("variable k hat den Wert :",k);
+document.write("variable l hat den Wert :",l);
+document.write("variable i hat den type: " ,typeof i);
+document.write("variable k hat den type: " ,typeof k);
+document.write("variable s hat den type: ",typeof s);
+document.write("variable j hat den type: ",typeof j);
+
+
+summe=i+l;
+document.write('die summe von i und l ist : ',summe);
